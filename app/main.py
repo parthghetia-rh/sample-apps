1f8a250354cd075860a87e567f6e90bedcd1e2c8
parthghetia-rh <146372326+parthghetia-rh@users.noreply.github.com>
2024-03-19 19:40:51 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return "good morning all who inhabit it!"
+  return "good morning world and all who inhabit it!"
 
 if __name__ == '__main__':
   port = 8080
