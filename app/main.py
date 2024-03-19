58051b574e2fd39852b2b11465edf687691fd8a5
parthghetia-rh <pghetia@redhat.com>
2024-03-19 15:48:57 -0400

    run

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return "good morning world and all who inhabit it!"
+  return "good morning and all who inhabit it!"
 
 if __name__ == '__main__':
   port = 8080
