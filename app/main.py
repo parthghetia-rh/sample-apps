f8d7d5793345e64e1b8fd9f267d920f4225254a9
parthghetia-rh <pghetia@redhat.com>
2024-03-19 21:15:25 -0400

    run

--- app/main.py
+++ app/main.py
@@ -1,13 +1,10 @@
-from flask import Flask, render_template_string
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    # Define the text to display
-    message = "Good morning all who inhabit it!!!!!!???"
-    # Render the template with the message
-    return render_template_string('<html><head><title>Beautiful Text</title></head><body><div style="font-size: 3em; text-align: center; margin-top: 20%;">{{ message }}</div></body></html>', message=message)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
