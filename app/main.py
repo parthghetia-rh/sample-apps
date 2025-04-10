0321b3b20b8404b72adb1bb67b1346aacdefa97f
parthghetia-rh <pghetia@redhat.com>
2025-04-10 13:28:02 -0400

    deploy new version

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def main():
-  return "People of this world!!!! Welcome Back!! This is 2025!!!!!! What's up"
+  return "People of this world!!!! Welcome Back!! This is 2025!!!!!!"
 
 if __name__ == '__main__':
   port = 8080
